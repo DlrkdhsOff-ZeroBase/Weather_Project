6ab7d60218dd06cd7419e002bb0b716038358769
Lee Kaon <dlrkdhsoff@gmail.com>
2024-06-23 19:12:24 +0900

    first commit

--- src/main/java/zero/weather/WeatherApplication.java
+++ src/main/java/zero/weather/WeatherApplication.java
@@ -1,5 +1,6 @@
 package zero.weather;
 
+import org.apache.tomcat.util.http.fileupload.MultipartStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
