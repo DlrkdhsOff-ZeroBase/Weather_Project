8b5516cce06ffcb29c162f3c5654e86e427edc58
Lee Kaon <dlrkdhsoff@gmail.com>
2024-06-25 04:39:48 +0900

    저장된 일기 조회

--- src/main/java/zero/weather/repository/DiaryRepository.java
+++ src/main/java/zero/weather/repository/DiaryRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import zero.weather.domain.Diary;
 
+import java.time.LocalDate;
+import java.util.*;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
-
+    List<Diary> findAllByDate(LocalDate date);
 }
